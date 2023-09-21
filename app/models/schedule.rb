742636b532e1eb2f354b2b526b08a3346d5b5933
danrayfr <danrayrollan98@gmail.com>
2023-09-21 10:13:57 +0800

    Added schedule notification

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -8,4 +8,19 @@ class Schedule < ApplicationRecord
   validates :end_time, presence: true
 
   enum :category, %i[coaching qa meeting]
+
+  has_many :notifications, through: :user, dependent: :destroy
+  has_noticed_notifications model_name: 'Notification'
+  after_create_commit :notify_recipient
+  before_destroy :clean_notifications
+
+  private
+
+  def notify_recipient
+    ScheduleNotification.with(schedule: self).deliver_later(participants)
+  end
+
+  def clean_notifications
+    notifications_as_ticket.destroy_all
+  end
 end
